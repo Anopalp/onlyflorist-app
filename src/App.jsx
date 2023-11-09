c1a61b9f9bc15c1e8543b31186fdeef1f3d415e2
samuel-eric <samueltan086@gmail.com>
2023-11-09 09:55:16 +0700

    pindahin fitur pengiriman manajer ke branch lain

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import DaftarPengirimanManajer from './DaftarPengirimanManajer'
-
 function App() {
 	return (
 		<div>
 			<h1>This is dashboard</h1>
-			<DaftarPengirimanManajer />
 		</div>
 	)
 }
