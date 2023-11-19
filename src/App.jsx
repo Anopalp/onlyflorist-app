ff5fe71d4e590efb46fc24cd6f3417e1df2c0c9c
luthfihanif10 <18221151@std.stei.itb.ac.id>
2023-11-19 22:55:01 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
+// import DaftarPengirimanManajer from './DaftarPengirimanManajer'
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import Appheader from './Appheader'
-import Login from './login'
+// import Appheader from './Appheader'
+// import Login from './login'
+// import DashboardKurir from './DashboardKurir'
+// import AccountKurir from './AccountKurir'
+// import DaftarPengirimanKurir from './DaftarPengirimanKurir'
+// import Create from './Create'
+// import Update from './Update'
+// import Read from './Read'
 import DashboardKurir from './DashboardKurir'
-import AccountKurir from './AccountKurir'
-import DaftarPengirimanKurir from './DaftarPengirimanKurir'
-import Create from './Create'
-import Update from './Update'
-import Read from './Read'
-import DashboardKurir from './DashboardKurir'
-import DashboardManager from './DashboardManager'
-
+import DashboardManajer from './DashboardManajer'
 
 function App() {
   
-  return (
-    <BrowserRouter>
-    <Appheader></Appheader>
-      <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='/dashboard-kurir' element={<DashboardKurir />}></Route>
-		<Route path='/dashboard-manager' element={<DashboardManager />}></Route>
-        <Route path='/daftar-pengiriman-kurir' element={<DaftarPengirimanKurir />}></Route>
-        <Route path='/account-kurir' element={<AccountKurir />}></Route>
-        <Route path='/create' element={<Create />}></Route>
-        <Route path='/daftar-pengiriman-kurir/update/:id' element={<Update />}></Route>
-        <Route path='/daftar-pengiriman-kurir/read/:id' element={<Read />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
-  
+	return (
+		<BrowserRouter>
+		{/* <Appheader></Appheader> */}
+		  <Routes>
+			{/* <Route path='/' element={<Login />}></Route> */}
+			<Route path='/' element={<DashboardKurir />}></Route>
+			{/* <Route path='/dashboard-manager' element={<DashboardManajer />}></Route>
+			<Route path='/daftar-pengiriman-kurir' element={<DaftarPengirimanKurir />}></Route>
+			<Route path='/account-kurir' element={<AccountKurir />}></Route>
+			<Route path='/create' element={<Create />}></Route>
+			<Route path='/daftar-pengiriman-kurir/update/:id' element={<Update />}></Route>
+			<Route path='/daftar-pengiriman-kurir/read/:id' element={<Read />}></Route> */}
+		  </Routes>
+		</BrowserRouter>
+	  )
+	  
+	
 }
 
 export default App
