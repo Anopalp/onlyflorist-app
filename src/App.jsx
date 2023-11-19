0e7bddf1d6b84f6cc5170a72124c7532a9a2e9d0
luthfihanif10 <18221151@std.stei.itb.ac.id>
2023-11-19 22:41:27 +0700

    Initial commit for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-// import DaftarPengirimanManajer from './DaftarPengirimanManajer'
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Appheader from './Appheader'
+import Login from './login'
 import DashboardKurir from './DashboardKurir'
-import DashboardManajer from './DashboardManajer'
+import AccountKurir from './AccountKurir'
+import DaftarPengirimanKurir from './DaftarPengirimanKurir'
+import Create from './Create'
+import Update from './Update'
+import Read from './Read'
 
 function App() {
-	return (
-		<div>
-			{/* <h1>This is dashboard</h1> */}
-			{/* <DaftarPengirimanManajer /> */}
-			<DashboardKurir />
-			<DashboardManajer />
-		</div>
-	)
+  
+  return (
+    <BrowserRouter>
+    <Appheader></Appheader>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/dashboard-kurir' element={<DashboardKurir />}></Route>
+        <Route path='/daftar-pengiriman-kurir' element={<DaftarPengirimanKurir />}></Route>
+        <Route path='/account-kurir' element={<AccountKurir />}></Route>
+        <Route path='/create' element={<Create />}></Route>
+        <Route path='/daftar-pengiriman-kurir/update/:id' element={<Update />}></Route>
+        <Route path='/daftar-pengiriman-kurir/read/:id' element={<Read />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+  
 }
 
 export default App
