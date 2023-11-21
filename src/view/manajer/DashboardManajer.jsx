7cf054e9e0215a6b1dcd0ad2c7b7157a34ffe0d9
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-11-21 08:45:24 +0700

    Nambahin dashboard Manajer

--- src/view/manajer/DashboardManajer.jsx
+++ src/view/manajer/DashboardManajer.jsx
@@ -1,72 +1,57 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function DashboardManajer() {
+  const [data, setData] = useState([])
+  const {id} = useParams();
+
+  useEffect(()=> {
+      axios.get('http://localhost:3000/pengiriman/' + id)
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div>
-      <p className='sapaan'>Hai nama, daftar pengiriman kamu hari ini adalah</p>
-      <div className='pengiriman-container'>
-        <div className='pengiriman-info'>
-          <div className='card-container'>
-            <div className='judul-container'>
-              <h3>Pengiriman yang masih berjalan</h3>
-            </div>
-            <div className='card-grid'>
-              <Card />
-              <Card />
+      <h4 class="text-center p-3">Halo Sumarno, daftar pengiriman kamu hari ini adalah </h4>
+      <div class="card-container w-50 mx-auto my-5">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="card w-75 text-center mx-auto my-5">
+              <img src="..\src\assets\Sansevieria.png" class="card-img-top" alt="Bunga" />
+              <div class="card-body">
+                <h5 class="card-title">Sansevieria</h5>
+                <p>Budi P</p>
+                <p>Jl. Tubagus Islamiyyah no.9</p>
+                <p>Sumarno</p>
+                <p>086542293839</p>
+                <a href="#" class="btn btn-primary">Lihat Detail</a>
+              </div>
             </div>
           </div>
-          <div className='buttons-container'>
-            <div>
-              <Button variant="secondary" className='ongoing-button'>Ongoing</Button>
-              <Button variant="secondary" className='done-button'>Done</Button>
+          <div class="col-lg-6">
+            <div class="card w-75 text-center mx-auto my-5">
+              <img src="..\src\assets\Sansevieria.png" class="card-img-top" alt="Bunga" />
+              <div class="card-body">
+                <h5 class="card-title">Sansevieria</h5>
+                <p>Carica Papaya</p>
+                <p>Jl. Tubagus Islamiyyah no.18</p>
+                <p>Sumarno</p>
+                <p>086542729739</p>
+                <a href="#" class="btn btn-primary">Lihat Detail</a>
+              </div>
             </div>
+          </div>
+          <div class="buttons-container">
             <div>
-              <Button variant="secondary" className='see-more-button'>See More</Button>
+              <Link to={'/daftar-pengiriman-kurir'} class='btn btn-secondary'>See More</Link>
             </div>
+            {/* <a href='/daftar-pengiriman-kurir' class="btn btn-primary">see more</a> */}
           </div>
         </div>
       </div>
     </div>
-  )
-}
-
-function Card(){
-  return (
-    <div className='card'>
-      <div className='card-info'>
-        <p>ID Pengiriman: 1368319827</p>
-        <p>Alamat: Jalan Tubagus Islamiyyah No.5</p>
-        <p>No Telp. Pelanggan: 0872639173887</p>
-      </div>
-      <ButtonDetailPengiriman/>
-    </div>
-  )
-}
-
-function ButtonDetailPengiriman() {
-  return (
-    <>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      <button className='button-detail-pengiriman'>
-        <span class="material-symbols-outlined">
-        arrow_forward_ios
-        </span>
-      </button>
-    </>
-  )
-}
-
-function ButtonDetailAkun() {
-  return (
-    <>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      <button className='button-detail-akun'>
-        <span class="material-symbols-outlined">
-        arrow_forward_ios
-        </span>
-      </button>
-    </>
   )
 }
 
