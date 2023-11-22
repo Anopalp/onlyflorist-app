0d4b466bcda5f5428e867c0fb41084ffb6622876
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-11-22 17:50:04 +0700

    Edit DashboardManager

--- src/view/manajer/DashboardManajer.jsx
+++ src/view/manajer/DashboardManajer.jsx
@@ -8,7 +8,7 @@ function DashboardManajer() {
       <div className='pengiriman-co'>
         <h4 class="text-center p-3">Halo MANAJER TERCINTAHH, selamat Pagi!</h4>
         <div class="card-container w-75 mx-auto my-5">
-          <div>
+          <div class="mx-5">
             <h3>Pengiriman teratas</h3>
           </div>
           <div class="row">
@@ -62,14 +62,14 @@ function DashboardManajer() {
       </div>
       <div className='akun-co'>
           <div class="card-container w-75 mx-auto my-5">
-            <div>
-              <h3 class="mx-5">Akun Kurir</h3>
+            <div class="mx-5">
+              <h3 class="text-center">Akun Kurir</h3>
             </div>
             <div class="row">
               <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -79,7 +79,7 @@ function DashboardManajer() {
                     </div>
                   </div>
                   <div class="carousel-item" >
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -89,7 +89,7 @@ function DashboardManajer() {
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -99,7 +99,7 @@ function DashboardManajer() {
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -108,8 +108,8 @@ function DashboardManajer() {
                       </div>
                     </div>
                   </div>
-                  <div class="carousel-item">
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                  {/* <div class="carousel-item">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -117,9 +117,9 @@ function DashboardManajer() {
                         <a href="#" class="btn btn-primary">Lihat Detail</a>
                       </div>
                     </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-100 h-100 text-center mx-auto my-auto">
+                  </div> */}
+                  {/* <div class="carousel-item">
+                    <div class="card w-75 h-100 text-center mx-auto my-auto">
                       <div class="card-body">
                         <h5 class="card-title">Cam Abbot</h5>
                         <p>cabbot0</p>
@@ -127,7 +127,7 @@ function DashboardManajer() {
                         <a href="#" class="btn btn-primary">Lihat Detail</a>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
--- src/view/manajer/DashboardManajerHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DashboardManajerHeader() {
-  return (
-    <div>DashboardManajerHeader</div>
-  )
-}
-
-export default DashboardManajerHeader
