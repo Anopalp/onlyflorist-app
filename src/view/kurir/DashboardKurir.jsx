4bc67626839f61982b0373fa002209e038685977
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-11-24 11:33:59 +0700

    Making Dashboard more Modular

--- src/view/kurir/DashboardKurir.jsx
+++ src/view/kurir/DashboardKurir.jsx
@@ -1,94 +1,35 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 import "../dashboardStyles.css";
-import { Link } from 'react-router-dom';
-import NavbarKurir from '../NavbarKurir';
+import { Link } from "react-router-dom";
+import NavbarKurir from "../NavbarKurir";
+import CardPengiriman from "../DashboardManajer";
 
 function DashboardKurir() {
   return (
     <div>
-      <NavbarKurir/>
+      <NavbarKurir />
       <h4 className="text-center p-3">Halo Sumarno, selamat Pagi!</h4>
       <div className="card-container w-75 mx-auto my-5">
-      <div>
+        <div>
           <h3>Pengiriman teratas</h3>
         </div>
         <div className="row">
-          <div className="col-lg-4">
-            <div className="card w-75 text-center mx-auto my-auto">
-              <img src="..\src\assets\Sansevieria.png" className="card-img-top" alt="Bunga" />
-              <div className="card-body">
-                <h5 className="card-title">Sansevieria</h5>
-                <p>Budi P</p>
-                <p>Jl. Tubagus Islamiyyah no.9</p>
-                <p>Sumarno</p>
-                <p>086542293839</p>
-                <a href="#" className="btn btn-primary">Lihat Detail</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="card w-75 text-center mx-auto my-auto">
-              <img src="..\src\assets\Sansevieria.png" className="card-img-top" alt="Bunga" />
-              <div className="card-body">
-                <h5 className="card-title">Sansevieria</h5>
-                <p>Carica Papaya</p>
-                <p>Jl. Tubagus Islamiyyah no.18</p>
-                <p>Sumarno</p>
-                <p>086542729739</p>
-                <a href="#" className="btn btn-primary">Lihat Detail</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="card w-75 text-center mx-auto my-auto">
-              <img src="..\src\assets\Sansevieria.png" className="card-img-top" alt="Bunga" />
-              <div className="card-body">
-                <h5 className="card-title">Sansevieria</h5>
-                <p>Carica Papaya</p>
-                <p>Jl. Tubagus Islamiyyah no.18</p>
-                <p>Sumarno</p>
-                <p>086542729739</p>
-                <a href="#" className="btn btn-primary">Lihat Detail</a>
-              </div>
-            </div>
-          </div>
+          <CardPengiriman />
+          <CardPengiriman />
+          <CardPengiriman />
           <div className="buttons-container mt-3">
             <div>
-              <Link to={'/daftar-pengiriman-kurir'} class='btn btn-secondary'>See More</Link>
+              <Link to={"/daftar-pengiriman-kurir"} class="btn btn-secondary">
+                See More
+              </Link>
             </div>
             {/* <a href='/daftar-pengiriman-kurir' class="btn btn-primary">see more</a> */}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-function Card(){
-  return (
-    <div className='card'>
-      <div className='card-info'>
-        <p>ID Pengiriman: 1368319827</p>
-        <p>Alamat: Jalan Tubagus Islamiyyah No.5</p>
-        <p>No Telp. Pelanggan: 0872639173887</p>
-      </div>
-      <ButtonDetailPengiriman/>
-    </div>
-  )
-}
-
-function ButtonDetailPengiriman() {
-  return (
-    <>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      <button className='button-detail-pengiriman'>
-        <span className="material-symbols-outlined">
-        arrow_forward_ios
-        </span>
-      </button>
-    </>
-  )
-}
-
-export default DashboardKurir
+export default DashboardKurir;
--- src/view/manajer/DashboardManajer.jsx
+++ src/view/manajer/DashboardManajer.jsx
@@ -4,65 +4,25 @@ import { Link, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import NavbarManajer from "../NavbarManajer";
-// import "react-multi-carousel/lib/styles.css";
 
 function DashboardManajer() {
   return (
     <div>
-      <NavbarManajer/>
+      <NavbarManajer />
       <div className="pengiriman-co">
         <h4 class="text-center text-danger p-3"></h4>
         <div class="card-container mx-auto my-5">
           <div class="mx-5">
-            <h3>Pengiriman teratas</h3>
+            <h2>Pengiriman teratas</h2>
           </div>
           <div class="row">
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Budi Pratama</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.18 Sumarno 086542729739
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Anita Disley</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.88 Sumarno 086541299739
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Alam Bar</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.15 Sumarno 086542728273
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
+            <CardPengiriman />
+            <CardPengiriman />
+            <CardPengiriman />
             <div class="buttons-container mt-3">
               <div>
-                <Link to={"/daftar-pengiriman-manajer"} class="btn">
-                  See More
+                <Link to={"/daftar-pengiriman-manajer"}>
+                  <Button variant="secondary"> See More </Button>
                 </Link>
               </div>
             </div>
@@ -72,55 +32,16 @@ function DashboardManajer() {
       <div className="akun-co">
         <div class="card-container mx-auto my-5">
           <div class="mx-5">
-            <h3 class="text-center">Akun Kurir</h3>
+            <h2 class="text-center">Akun Kurir</h2>
           </div>
           <div class="row">
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Budi Pratama</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.18 Sumarno 086542729739
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Anita Disley</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.88 Sumarno 086541299739
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-            <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
-              <Card.Img
-                variant="top"
-                src="..\src\assets\Sansevieria.png"
-              ></Card.Img>
-              <Card.Header>Sansevieria</Card.Header>
-              <Card.Body>
-                <Card.Title>Alam Bar</Card.Title>
-                <Card.Text>
-                  Jl. Tubagus Islamiyyah no.15 Sumarno 086542728273
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
+            <CardKurir />
+            <CardKurir />
+            <CardKurir />
             <div class="buttons-container mt-3">
               <div>
-                <Link to={"/daftar-pengiriman-manajer"} class="btn">
-                  See More
+                <Link to={"/daftar-kurir"}>
+                  <Button variant="secondary"> See More </Button>
                 </Link>
               </div>
             </div>
@@ -128,6 +49,33 @@ function DashboardManajer() {
         </div>
       </div>
     </div>
+  );
+}
+
+function CardPengiriman() {
+  return (
+    <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
+      <Card.Img variant="top" src="..\src\assets\Sansevieria.png"></Card.Img>
+      <Card.Header>Sansevieria</Card.Header>
+      <Card.Body>
+        <Card.Title>Alam Bar</Card.Title>
+        <Card.Text>Jl. Tubagus Islamiyyah no.15 Sumarno 086542728273</Card.Text>
+        <Button variant="primary">Lihat Detail</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function CardKurir() {
+  return (
+    <Card className="text-center w-25 col-lg-4 mx-auto my-auto">
+      <Card.Header>Suharmono</Card.Header>
+      <Card.Body>
+        <Card.Title>08273627469</Card.Title>
+        <Card.Text>Jl. Tubagus Islamiyyah no.77</Card.Text>
+        <Button variant="primary">Lihat Detail</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
