418d1e459bf3893b98c4723588a2f0ad0f80913b
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-11-27 12:17:22 +0700

    Update sizing login page

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,61 +1,60 @@
-import Avatar from '@mui/material/Avatar'
-import CssBaseline from '@mui/material/CssBaseline'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { Navigate } from 'react-router-dom'
-import supabase from '../config/supabaseClient'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import NavbarLogin from './NavbarLogin'
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Navigate } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import NavbarLogin from "./NavbarLogin";
 
 export default function Login({ session }) {
-	if (!session) {
-		return (
-			<div>
-				<NavbarLogin/>
+  if (!session) {
+    return (
+      <div>
+        <NavbarLogin />
 
-				<Container component='main' maxWidth='xs'>
-				
-				<CssBaseline />
-				<Box
-					sx={{
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-						justifyContent: 'center',
-						minHeight: '75vh',
-					}}
-				>
-					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography
-						component='h1'
-						variant='h5'
-						style={{
-							display: 'flex',
-							justifyContent: 'center',
-							fontWeight: 'bold',
-						}}
-					>
-						Login
-					</Typography>
-					<Auth
-						supabaseClient={supabase}
-						providers={[]}
-						appearance={{ theme: ThemeSupa }}
-					/>
-				</Box>
-			</Container>
-			</div>
-		)
-	} else {
-		if (session.user.email === 'manajer@fakemail.com') {
-			return <Navigate to='/dashboard-manajer' />
-		} else {
-			return <Navigate to='/dashboard-kurir' />
-		}
-	}
+        <Container className="min-vh-100" component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              minHeight: "75vh",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography
+              component="h1"
+              variant="h5"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                fontWeight: "bold",
+              }}
+            >
+              Login
+            </Typography>
+            <Auth
+              supabaseClient={supabase}
+              providers={[]}
+              appearance={{ theme: ThemeSupa }}
+            />
+          </Box>
+        </Container>
+      </div>
+    );
+  } else {
+    if (session.user.email === "manajer@fakemail.com") {
+      return <Navigate to="/dashboard-manajer" />;
+    } else {
+      return <Navigate to="/dashboard-kurir" />;
+    }
+  }
 }
