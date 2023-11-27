4ce85996ecb6fa9e7d4f3b41c3a8ded4f8eac057
samuel-eric <samueltan086@gmail.com>
2023-11-27 11:14:48 +0700

    update ui update dan footer

--- src/view/footer.jsx
+++ src/view/footer.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { MDBFooter } from 'mdb-react-ui-kit';
+import { MDBFooter } from 'mdb-react-ui-kit'
 
 export default function Footer() {
-  return (
-    <div>
-      <MDBFooter className='text-center text-lg-start text-muted my-auto' style={{ backgroundColor: 'rgb(41, 51, 92)' }}>
-        <div className='footer-text text-center p-3'>
-          © 2023 Copyright:
-          <a className="text-reset fw-bold" href="/about">OnlyFlorist</a>
-        </div>
-      </MDBFooter>
-    </div>
-  );
+	return (
+		<div>
+			<MDBFooter
+				className='text-center text-lg-start text-muted my-auto'
+				style={{ backgroundColor: 'rgb(41, 51, 92)' }}
+			>
+				<div className='footer-text text-center p-3'>
+					© 2023 Copyright: <strong>OnlyFlorist</strong>
+				</div>
+			</MDBFooter>
+		</div>
+	)
 }
