ec65afef3645980646b4117dbc19bca8f765e7da
Aliefnaufal <18221105@std.stei.itb.ac.id>
2023-11-27 11:42:26 +0700

    Update Update.jsx

--- src/view/kurir/Update.jsx
+++ src/view/kurir/Update.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import supabase from '../../config/supabaseClient'
@@ -61,8 +62,6 @@ function Update() {
 	const navigate = useNavigate()
 
 	const [values, setValues] = useState({})
-	const [isLoading, setIsLoading] = useState(false)
-	const [isUpdating, setIsUpdating] = useState(false)
 
 	const options = [
 		{ label: 'Pick-up', mark: 1 },
@@ -73,7 +72,6 @@ function Update() {
 
 	useEffect(() => {
 		const fetchData = async () => {
-			setIsLoading(true)
 			const { data, error } = await supabase
 				.from('dataPengiriman')
 				.select()
@@ -94,7 +92,6 @@ function Update() {
 			} else {
 				setValues(...data)
 			}
-			setIsLoading(false)
 		}
 
 		fetchData()
@@ -103,23 +100,26 @@ function Update() {
 	const handleUpdate = (event) => {
 		event.preventDefault()
 		const updateData = async () => {
-			setIsUpdating(true)
 			await updateStatusPengiriman(id, values.status_pengiriman)
 			await updateLaporan(values.laporan_masalah, id, values.id_laporan)
-			setIsUpdating(false)
 		}
 
-		updateData()
+		updateData();
 
-		const backButton = document.querySelector('.back-button');
-		backButton.click();
+		// document.querySelector('.back-button').click();
+
+		navigate('/daftar-pengiriman-kurir')
+
+		setTimeout(() => {
+			window.location.reload();
+		}, 500);
 	}
 
 	return (
 		<div>
 			<NavbarKurir />
 			<div className='d-flex w-100 vh-75 justify-content-center align-items-center'>
-				<div className='w-50 bg-white p-3 rounded-4 my-3'>
+				<div className='w-50 bg-white p-3 rounded-4'>
 					<div className='d-flex align-items-center p-1 pb-4 border-bottom-0'>
 						<Link to={'/daftar-pengiriman-kurir/'}>
 							<button type='button' className='btn back-button'>
@@ -137,120 +137,112 @@ function Update() {
 							Update Pengiriman
 						</h3>
 					</div>
-					{isLoading ? (
-						<div className='d-flex justify-content-center py-5'>
-							<div className='spinner-border' role='status'></div>
-						</div>
-					) : (
-						<form onSubmit={handleUpdate}>
-							<div className='d-flex align-items-center mb-4'>
-								<img
-									style={{ width: '120px', borderRadius: '50%' }}
-									src={values.image_url}
-									alt='profile picture'
-								></img>
-								<h3
-									style={{
-										fontSize: 30,
-										fontWeight: 'bold',
-										color: '#29335c',
-										marginLeft: '10%',
-									}}
-								>
-									{values.jenis_bunga}
-								</h3>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='id' style={{ color: '#29335c' }}>
-									ID Pengiriman
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.id}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='alamatPengiriman' style={{ color: '#29335c' }}>
-									Alamat Pengiriman
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.alamat_pengiriman}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='jenisBunga' style={{ color: '#29335c' }}>
-									Jenis Bunga
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.jenis_bunga}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='noTelpPelanggan' style={{ color: '#29335c' }}>
-									No. Telp Pelanggan
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.nomor_telp_pelanggan}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='catatan' style={{ color: '#29335c' }}>
-									Catatan
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.catatan ? values.catatan : '-'}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='kurir' style={{ color: '#29335c' }}>
-									Kurir
-								</strong>
-								<output className='form-control' style={{ color: '#29335c' }}>
-									{values.kurir}
-								</output>
-							</div>
-							<div className='mb-3'>
-								<strong htmlFor='laporanMasalah' style={{ color: '#29335c' }}>
-									Laporan Masalah
-								</strong>
-								<input
-									type='text'
-									name='name'
-									className='form-control'
-									style={{ color: '#29335c' }}
-									placeholder='Masukkan laporan masalah'
-									value={values.laporan_masalah || ''}
-									onChange={(e) =>
-										setValues({ ...values, laporan_masalah: e.target.value })
-									}
-									disabled={values.status_pengiriman !== 'On-hold'}
-								/>
-							</div>
-							<div className='mb-4'>
-								<strong htmlFor='status' style={{ color: '#29335c' }}>
-									Status Pengiriman
-								</strong>
-								<select
-									name='status'
-									className='form-select'
-									style={{ color: '#29335c' }}
-									value={values.status_pengiriman}
-									onChange={(e) =>
-										setValues({ ...values, status_pengiriman: e.target.value })
-									}
-								>
-									{options.map((option) => (
-										<option key={option.mark}>{option.label}</option>
-									))}
-								</select>
-							</div>
-							<button
-								className='btn rounded-3 btn-success w-100'
-								disabled={isUpdating}
+					<form onSubmit={handleUpdate}>
+						<div className='d-flex align-items-center mb-4'>
+							<img
+								style={{ width: '120px', borderRadius: '50%' }}
+								src={values.image_url}
+								alt='profile picture'
+							></img>
+							<h3
+								style={{
+									fontSize: 30,
+									fontWeight: 'bold',
+									color: '#29335c',
+									marginLeft: '10%',
+								}}
 							>
-								{isUpdating ? 'Updating...' : 'Update'}
-							</button>
-						</form>
-					)}
+								{values.jenis_bunga}
+							</h3>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='id' style={{ color: '#29335c' }}>
+								ID Pengiriman
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.id}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='alamatPengiriman' style={{ color: '#29335c' }}>
+								Alamat Pengiriman
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.alamat_pengiriman}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='jenisBunga' style={{ color: '#29335c' }}>
+								Jenis Bunga
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.jenis_bunga}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='noTelpPelanggan' style={{ color: '#29335c' }}>
+								No. Telp Pelanggan
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.nomor_telp_pelanggan}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='catatan' style={{ color: '#29335c' }}>
+								Catatan
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.catatan ? values.catatan : '-'}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='kurir' style={{ color: '#29335c' }}>
+								Kurir
+							</strong>
+							<output className='form-control' style={{ color: '#29335c' }}>
+								{values.kurir}
+							</output>
+						</div>
+						<div className='mb-3'>
+							<strong htmlFor='laporanMasalah' style={{ color: '#29335c' }}>
+								Laporan Masalah
+							</strong>
+							<input
+								type='text'
+								name='name'
+								className='form-control'
+								style={{ color: '#29335c' }}
+								placeholder='Masukkan laporan masalah'
+								value={values.laporan_masalah || ''}
+								onChange={(e) =>
+									setValues({ ...values, laporan_masalah: e.target.value })
+								}
+								disabled={values.status_pengiriman !== 'On-hold'}
+							/>
+						</div>
+						<div className='mb-4'>
+							<strong htmlFor='status' style={{ color: '#29335c' }}>
+								Status Pengiriman
+							</strong>
+							{/* <input type="text" name='status' className='form-control' placeholder='Masukkan Status'
+                    value={values.statusPengiriman}
+                    onChange={e => setValues({...values, statusPengiriman: e.target.value})}/> */}
+							<select
+								name='status'
+								className='form-select'
+								style={{ color: '#29335c' }}
+								value={values.status_pengiriman}
+								onChange={(e) =>
+									setValues({ ...values, status_pengiriman: e.target.value })
+								}
+							>
+								{options.map((option) => (
+									<option key={option.mark}>{option.label}</option>
+								))}
+							</select>
+						</div>
+						<button className='btn rounded-3 btn-success w-100'>Update</button>
+					</form>
 				</div>
 			</div>
 		</div>
