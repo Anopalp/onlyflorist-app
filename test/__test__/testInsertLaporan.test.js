66b7a9ca44912f17565c558f7351b96c2ff3a31f
luthfihanif10 <18221151@std.stei.itb.ac.id>
2023-11-27 18:23:54 +0700

    Fix test udah kelar

--- test/__test__/testInsertLaporan.test.js
+++ test/__test__/testInsertLaporan.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import insertLaporan from "../__test__/testInsertLaporan.test";
+import insertLaporan from "../component/testInsertLaporan";
 
 test("Test Fitur Insert Pengiriman", async () => {
     const result = await insertLaporan("Bunga Galak!", 1000);
